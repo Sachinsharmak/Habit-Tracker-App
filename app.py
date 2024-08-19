7f31b3a1b5882a28153b7ebbd3cc9abdf20bbdb4
Sachinsharmak <89272sachin1@gmail.com>
2024-08-19 22:59:09 +0530

    Changes

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(pages)
+    
+    # Configure the MongoDB connection
     client = MongoClient(os.environ.get("MONGO_URI"))
     
     # Use the specified database name from the environment variable
@@ -19,3 +21,10 @@ def create_app():
     app.db = client[db_name]  # Access the database by name
 
     return app
+
+# Create an instance of the app
+app = create_app()
+
+# Ensure the Flask app runs when the script is executed
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
